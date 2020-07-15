caf2459f979f108caa3cfff656588ceacd0a1276
Mahfujur51 <overcastmoon@gmail.com>
2020-07-15 10:50:07 +0600

    initial

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
